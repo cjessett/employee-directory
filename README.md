9e3f8bde5e6e2e99703d713d365b4f4d0eea4b71
CJ Jessett <cjessett@gmail.com>
2019-05-13 13:25:26 -0700

    Make page list responsive

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Employee directory
+# Employee Directory
 
 ## Getting started
 #### Install dependencies
@@ -10,23 +10,46 @@ For the client
 cd client && yarn
 ```
 
+##### For Development
 Setup the database
 ```
 yarn db:setup
 ```
 
-#### Starting the application
-
-For production
-```
-yarn build
-```
-
-```
-yarn start
-```
-
-For development
+Start the App
 ```
 yarn server & yarn client
 ```
+
+##### For Production
+
+Ensure a PGSQL database exists
+```
+// on a mac
+createdb employee_directory
+```
+Setup the database
+```
+NODE_ENV=production yarn db:setup
+```
+Build the client
+```
+yarn build
+```
+Start the server
+```
+NODE_ENV=production yarn start
+```
+Visit http://localhost:3001
+
+## Architecture
+The client was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and uses [React Router](https://reacttraining.com/react-router/) for routing and [Redux](https://redux.js.org) for state management.
+
+The server uses [Express](http://expressjs.com/) and [knex](https://knexjs.org/) to interact with the Postgres or SQLite database.
+
+### Features
+- Pagination
+- Ability to filter employees by department, title, or location. 
+- Search for employees on the page by name.
+
+
--- client/src/components/Paginator.js
+++ client/src/components/Paginator.js
@@ -9,39 +9,41 @@ function Paginator({ page, pages, updatePage }) {
     updatePage(page);
     window.scrollTo(0, 0)
   }
-  const pageLinks = getVisiblePages(page, pages).map(p => {
+  const pageLinks = getVisiblePages(window.screen.width)(page, pages).map(p => {
     const style = p === page ? { pointerEvents: 'none', color: 'inherit' } : {};
-    return <a key={p} href="" onClick={handleClick(p)} className="m-1" style={style}>{p}</a>
+    return <button key={p} href="" onClick={handleClick(p)} className="btn btn-link" style={style}>{p}</button>
   });
   return (
     <div>
-      {page > 1 && <button onClick={handleClick(page - 1)} className="btn btn-outline-primary m-1" type="button">Previous</button>}
+      {page > 1 && <button onClick={handleClick(page - 1)} className="btn btn-link m-1" type="button">Previous</button>}
       <span className="m-3">{pageLinks}</span>
-      {page < pages && <button onClick={handleClick(page + 1)} className="btn btn-outline-primary m-1" type="button">Next</button>}
+      {page < pages && <button onClick={handleClick(page + 1)} className="btn btn-link m-1" type="button">Next</button>}
     </div>
   )
 }
 
-function getVisiblePages(currentPage, totalPages) {
-  const limit = 10;
-  const offset = Math.ceil(limit / 2);
-  let start = currentPage - offset;
-  let end = currentPage + offset;
-  
-  if (totalPages <= limit) {
-    start = 0;
-    end = totalPages;
-  } else if (currentPage <= offset) {
-    start = 0;
-    end = limit;
-  } else if ((currentPage + offset) >= totalPages) {
-    start = totalPages - limit;
-    end = totalPages;
+function getVisiblePages(width) {
+  const limit = width < 400 ? 4 : 10;
+  return function (currentPage, totalPages) {
+    const offset = Math.ceil(limit / 2);
+    let start = currentPage - offset;
+    let end = currentPage + offset;
+    
+    if (totalPages <= limit) {
+      start = 0;
+      end = totalPages;
+    } else if (currentPage <= offset) {
+      start = 0;
+      end = limit;
+    } else if ((currentPage + offset) >= totalPages) {
+      start = totalPages - limit;
+      end = totalPages;
+    }
+
+    const range = (s, e) => [...Array(e - s).keys()].map(i => i + 1 + s);
+
+    return range(start, end);
   }
-
-  const range = (s, e) => [...Array(e - s).keys()].map(i => i + 1 + s);
-
-  return range(start, end);
 }
 
 function mapStateToProps({ employees: { page, pages } }) {
